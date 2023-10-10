f938307533e75641b3af2c02f2e9824a77812c90
Ferissa Mabalot <mabalot.ferissa@gmail.com>
2023-10-09 17:37:32 -0700

    html start

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// *
+// - Ask the player to enter the amount of coins he/she wants to insert into the machine.
+// - Generate the three digits and show them to the user (hint: you will need to generate each digit separately.)
+// - Any 3 matching digits = Win ten 10 times the amount of the original bet
+// - Any 2 matching digits = Win 2 times the amount of the original bet
+// - Anything else = Lose your bet
+// - Show the player how much they won or lost and how much coins they have remaining
+// - Allow the player to play again, but only if they have coins left
+// - When the player ran out of coins, a "thank you" message will pop up and end the game.
