4d53e9d0e44c6d0c9ca9e6f5a75d22c73fed00c3
Ferissa Mabalot <mabalot.ferissa@gmail.com>
2023-10-13 02:09:43 -0700

    js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// *
+// - Ask the player to enter the amount of coins he/she wants to insert into the machine.
+// - Generate the three digits and show them to the user (hint: you will need to generate each digit separately.)
+// - Any 3 matching digits = Win ten 10 times the amount of the original bet
+// - Any 2 matching digits = Win 2 times the amount of the original bet
+// - Anything else = Lose your bet
+// - Show the player how much they won or lost and how much coins they have remaining
+// - Allow the player to play again, but only if they have coins left
+// - When the player ran out of coins, a "thank you" message will pop up and end the game.
+
+
+function getElement (id) {
+    return document.getElementById(id);
+}
+
+function spin () {
+    const item1 = getElement ('wheel1');
+    const item2 = getElement ('wheel2');
+    const item3 = getElement ('wheel3');
+
+    const number1 = getRandomNumber ();
+    const number2 = getRandomNumber ();
+    const number3 = getRandomNumber ();
+
+    item1.innerHTML = `${number1}`;
+    item2.innerHTML = `${number1}`;
+    item3.innerHTML = `${number1}`;
+
+    if (number1 === number2 && number1 === number3) {
+        playPing();
+        showMessage();
+    } else {
+        hideMessage();
+    }
+
+    function playPing(){
+        const ping = document.getElementById("ping");
+        ping.play();
+    }
+
+    function showMessage(){
+        const message = document.getElementById('message');
+        message.style.display = 'block';
+    }
+
+    function hideMessage(){
+        const message = document.getElementById('message');
+        message.style.display = 'none';
+    }
+}
