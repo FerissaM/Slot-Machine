7e34981d6775834f146ecb8c0ba7db815e84d152
Ferissa Mabalot <mabalot.ferissa@gmail.com>
2023-10-16 05:19:49 -0700

    updated js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
 // *
-// - Ask the player to enter the amount of coins he/she wants to insert into the machine.
-// - Generate the three digits and show them to the user (hint: you will need to generate each digit separately.)
-// - Any 3 matching digits = Win ten 10 times the amount of the original bet
-// - Any 2 matching digits = Win 2 times the amount of the original bet
-// - Anything else = Lose your bet
-// - Show the player how much they won or lost and how much coins they have remaining
-// - Allow the player to play again, but only if they have coins left
-// - When the player ran out of coins, a "thank you" message will pop up and end the game.
+
+let bet, wallet, msg
+const spinBtn = document.querySelector("#spin-btn")
+const betMsg = document.querySelector("#bet")
+const betInput = document.querySelector("input")
+const walletMsg = document.querySelector("#wallet")
+const placeBetBtn = document.querySelector("#place-bet")
+const message = document.getElementById('message');
+const resetBtn = document.querySelector("#reset")
+console.log(betInput)
+spinBtn.addEventListener("click", spin)
+placeBetBtn.addEventListener("click", placeBet)
+resetBtn.addEventListener("click", init)
+
+function placeBet() {
+    if (betInput.value <= 0||wallet <= 0) return
+    console.log(typeof betInput.value)
+    bet = parseInt(betInput.value) // parseInt turns string to number
+    wallet = wallet-bet
+    msg = "Spin The Wheel!"
+    render()
+}
+
+function render(){
+    betMsg.textContent = `$${bet}`
+    betInput.value = 0
+    walletMsg.textContent = `$${wallet}`
+    message.textContent = msg
+}
 
 
-
+function init(){
+    bet = 0
+    wallet = 100
+    msg = "Place Your Bet!"
+    render()
+}
 
 function getRandomNumber() {
     return Math.floor(Math.random() * 2) + 1;
@@ -20,6 +46,7 @@ function getElement (id) {
 }
 
 function spin () {
+    if (bet <= 0) return 
     const wheel1 = getElement ('wheel1');
     const wheel2 = getElement ('wheel2');
     const wheel3 = getElement ('wheel3');
@@ -32,19 +59,21 @@ function spin () {
     wheel2.innerHTML = `${number2}`;
     wheel3.innerHTML = `${number3}`;
 
-    if (number1 === number2 && number1 === number3) {
-        showMessage();
+    if (number1 === number2 && number1 === number3 && number2 === number3) {
+        msg = "You win"
+        const amountWon = bet * 2
+        wallet = wallet + amountWon
+        bet = 0
+        render()
     } else {
-        hideMessage();
-    }
-
-    function showMessage(){
-        const message = document.getElementById('message');
-        message.style.display = 'block';
-    }
-
-    function hideMessage(){
-        const message = document.getElementById('message');
-        message.style.display = 'none';
+        console.log(wallet)
+        msg = "Mismatch!"
+        bet = 0
+        if (wallet <= 0){
+            msg = "You Lose!"
+        }
+        render()
     }
 }
+
+init()
